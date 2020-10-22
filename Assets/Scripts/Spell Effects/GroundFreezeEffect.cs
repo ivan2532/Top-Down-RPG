b5ee41b9b1dc5e755de7d2693997fed25f3ea22f
ivan2532 <ivan.bozovic02@gmail.com>
2020-10-23 01:39:28 +0200

    Ground Freeze Effect

---
+++ Assets/Scripts/Spell Effects/GroundFreezeEffect.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public class GroundFreezeEffect : MonoBehaviour
+{
+    private ShardController[] shards;
+    private Material myMaterial;
+
+    private bool effectInProgress = false;
+
+    private float effectDuration = 1.0f;
+    private float areaAlphaSmoothness;
+
+    private void Awake()
+    {
+        shards = GetComponentsInChildren<ShardController>();
+        myMaterial = GetComponent<Renderer>().material;
+    }
+
+    private void Update()
+    {
+        float newAlpha = Mathf.SmoothDamp(myMaterial.color.a, effectInProgress ? 0.04f : 0.0f, ref areaAlphaSmoothness, effectInProgress ? 0.2f : 0.05f);
+        myMaterial.color = new Color(myMaterial.color.r, myMaterial.color.g, myMaterial.color.b, newAlpha);
+
+        if (Input.GetKeyDown(KeyCode.T))
+            PlayEffect(0.5f);
+    }
+
+    public void PlayEffect(float duration)
+    {
+        effectDuration = duration;
+        effectInProgress = true;
+
+        foreach (var shard in shards)
+            shard.PlayEffect();
+
+        StartCoroutine(EndEffect());
+    }
+
+    private IEnumerator EndEffect()
+    {
+        yield return new WaitForSeconds(effectDuration);
+
+        effectInProgress = false;
+
+        foreach (var shard in shards)
+            shard.EndEffect();
+    }
+}
---
+++ Assets/Scripts/Spell Effects/ShardController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class ShardController : MonoBehaviour
+{
+    public const float scaleRandomness = 0.03f;
+    public const float startingScale = 0.125f;
+
+    private float targetScale;
+    private Quaternion targetRotation;
+
+    private Vector3 scaleSmoothness;
+    private Quaternion rotationSmoothness;
+
+    private float scaleSmoothAmount = 0.2f;
+    private float rotationSmoothAmount = 0.2f;
+
+    private void Start()
+    {
+        transform.localScale = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        transform.localScale = Vector3.SmoothDamp(transform.localScale, new Vector3(targetScale, targetScale, targetScale * 1.8f), ref scaleSmoothness, scaleSmoothAmount);
+        transform.rotation = SmoothDampUtility.QuaternionSmoothDamp(transform.rotation, targetRotation, ref rotationSmoothness, rotationSmoothAmount);
+    }
+
+    public void PlayEffect()
+    {
+        scaleSmoothAmount = 0.05f;
+        rotationSmoothAmount = 0.05f;
+
+        targetRotation = Quaternion.Euler(90.0f, Random.Range(0.0f, 360.0f), 180.0f);
+        targetScale = Random.Range(startingScale - scaleRandomness, startingScale + scaleRandomness);
+    }
+
+    public void EndEffect()
+    {
+        scaleSmoothAmount = 0.05f;
+        rotationSmoothAmount = 0.04f;
+
+        targetRotation = Quaternion.Euler(90.0f, 0.0f, 180.0f);
+        targetScale = 0.0f;
+    }
+}
