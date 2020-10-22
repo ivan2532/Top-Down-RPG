79a5a3da623bb94f1a2d97781bf2ce1d418e789f
ivan2532 <ivan.bozovic02@gmail.com>
2020-10-22 11:15:11 +0200

    Player Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 5.0f;
+
+    private float inputX, inputY;
+
+    private Vector3 cameraPlayerY;
+    private Vector3 forwardDirection;
+    private Vector3 rightDirection;
+
+    private float currentSpeed = 0.0f;
+    Vector3 lastPosition = Vector3.zero;
+
+    private Quaternion rotationSmoothVelocity;
+
+    private Rigidbody myRigidbody;
+    private Animator myAnimator;
+
+    private void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+        myAnimator = GetComponent<Animator>();
+
+        CalculateAxes();
+    }
+
+    private void FixedUpdate()
+    {
+        currentSpeed = (transform.position - lastPosition).sqrMagnitude;
+        lastPosition = transform.position;
+
+        Vector3 movementInput = new Vector3(inputX, 0.0f, inputY).normalized * movementSpeed;
+        myRigidbody.velocity = forwardDirection * movementInput.z + rightDirection * movementInput.x + new Vector3(0.0f, myRigidbody.velocity.y);
+        myAnimator.SetFloat("Speed", currentSpeed / 0.01f);
+    }
+
+    private void Update()
+    {
+        inputX = Input.GetAxisRaw("Horizontal");
+        inputY = Input.GetAxisRaw("Vertical");
+
+        if (myRigidbody.velocity.sqrMagnitude > movementSpeed * movementSpeed / 2.0f)
+        {
+            Quaternion newRotation = Quaternion.LookRotation(myRigidbody.velocity);
+            newRotation.eulerAngles = new Vector3(0.0f, newRotation.eulerAngles.y);
+            Quaternion smoothedRotation = SmoothDampUtility.QuaternionSmoothDamp(transform.rotation, newRotation, ref rotationSmoothVelocity, 0.02f);
+            myRigidbody.MoveRotation(smoothedRotation);
+        }
+    }
+
+    private void CalculateAxes()
+    {
+        Vector3 mainCameraPosition = Camera.main.transform.position;
+        cameraPlayerY = new Vector3(mainCameraPosition.x, transform.position.y, mainCameraPosition.z);
+        forwardDirection = (transform.position - cameraPlayerY).normalized;
+        rightDirection = Vector3.Cross(Vector3.up, forwardDirection);
+    }
+}
---
+++ Assets/Scripts/SmoothDampUtility.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public static class SmoothDampUtility
+{
+	public static Quaternion QuaternionSmoothDamp(Quaternion current, Quaternion target, ref Quaternion smoothVelocity, float smoothTime)
+	{
+		if (Time.deltaTime < Mathf.Epsilon)
+			return current;
+
+		float Dot = Quaternion.Dot(current, target);
+		float Multi = Dot > 0f ? 1f : -1f;
+		target.x *= Multi;
+		target.y *= Multi;
+		target.z *= Multi;
+		target.w *= Multi;
+
+		Vector4 Result = new Vector4(
+			Mathf.SmoothDamp(current.x, target.x, ref smoothVelocity.x, smoothTime),
+			Mathf.SmoothDamp(current.y, target.y, ref smoothVelocity.y, smoothTime),
+			Mathf.SmoothDamp(current.z, target.z, ref smoothVelocity.z, smoothTime),
+			Mathf.SmoothDamp(current.w, target.w, ref smoothVelocity.w, smoothTime)
+		).normalized;
+
+
+		Vector4 derivError = Vector4.Project(new Vector4(smoothVelocity.x, smoothVelocity.y, smoothVelocity.z, smoothVelocity.w), Result);
+		smoothVelocity.x -= derivError.x;
+		smoothVelocity.y -= derivError.y;
+		smoothVelocity.z -= derivError.z;
+		smoothVelocity.w -= derivError.w;
+
+		return new Quaternion(Result.x, Result.y, Result.z, Result.w);
+	}
+
+    public static Color ColorSmoothDamp(Color current, Color target, ref Color smoothVelocity, float smoothTime)
+    {
+        return new Color(
+            Mathf.SmoothDamp(current.r, target.r, ref smoothVelocity.r, smoothTime),
+            Mathf.SmoothDamp(current.g, target.g, ref smoothVelocity.g, smoothTime),
+            Mathf.SmoothDamp(current.b, target.b, ref smoothVelocity.b, smoothTime),
+            Mathf.SmoothDamp(current.a, target.a, ref smoothVelocity.a, smoothTime)
+		);
+    }
+}
