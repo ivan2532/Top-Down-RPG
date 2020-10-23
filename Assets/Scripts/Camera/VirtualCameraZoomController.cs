470651fcc4d18be6f5ca563307c1829a0242d58d
ivan2532 <ivan.bozovic02@gmail.com>
2020-10-23 23:05:00 +0200

    Smoothned zooming

--- Assets/Scripts/Camera/VirtualCameraZoomController.cs
+++ Assets/Scripts/Camera/VirtualCameraZoomController.cs
@@ -3,16 +3,21 @@ using UnityEngine;
 
 public class VirtualCameraZoomController : MonoBehaviour
 {
-    [SerializeField] private float zoomSensitivity = 0.3f;
+    [SerializeField] private float zoomSensitivity = 0.5f;
+    [SerializeField] private float zoomSmoothness = 0.1f;
     [SerializeField] private float minOrthographicSize = 2f;
     [SerializeField] private float maxOrthographicSize = 7f;
 
     private CinemachineVirtualCamera virtualCamera;
     private float scrollDelta = 0f;
+    private float orthographicSizeSmoothVelocity;
+
+    private float newOrthographicSize;
 
     private void Start()
     {
         virtualCamera = GetComponent<CinemachineVirtualCamera>();
+        newOrthographicSize = virtualCamera.m_Lens.OrthographicSize;
     }
 
     private void Update()
@@ -21,10 +26,15 @@ public class VirtualCameraZoomController : MonoBehaviour
 
         if (scrollDelta != 0.0f)
         {
-            virtualCamera.m_Lens.OrthographicSize = Mathf.Clamp(
+            newOrthographicSize = Mathf.Clamp(
                 virtualCamera.m_Lens.OrthographicSize + scrollDelta * zoomSensitivity,
                 minOrthographicSize,
                 maxOrthographicSize);
         }
+
+        virtualCamera.m_Lens.OrthographicSize = Mathf.SmoothDamp(
+            virtualCamera.m_Lens.OrthographicSize,
+            newOrthographicSize,
+            ref orthographicSizeSmoothVelocity, zoomSmoothness);
     }
 }
