1c3ae1ab77247492042b35a4454c295ba0b319e5
Neterskian <neterskian@gmail.com>
2020-10-23 22:44:09 +0200

    Added scrollwheel zoom

---
+++ Assets/Scripts/Camera/VirtualCameraZoomController.cs
@@ -0,0 +1,30 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+public class VirtualCameraZoomController : MonoBehaviour
+{
+    [SerializeField] private float zoomSensitivity = 0.3f;
+    [SerializeField] private float minOrthographicSize = 2f;
+    [SerializeField] private float maxOrthographicSize = 7f;
+
+    private CinemachineVirtualCamera virtualCamera;
+    private float scrollDelta = 0f;
+
+    private void Start()
+    {
+        virtualCamera = GetComponent<CinemachineVirtualCamera>();
+    }
+
+    private void Update()
+    {
+        scrollDelta = -Input.mouseScrollDelta.y;
+
+        if (scrollDelta != 0.0f)
+        {
+            virtualCamera.m_Lens.OrthographicSize = Mathf.Clamp(
+                virtualCamera.m_Lens.OrthographicSize + scrollDelta * zoomSensitivity,
+                minOrthographicSize,
+                maxOrthographicSize);
+        }
+    }
+}
