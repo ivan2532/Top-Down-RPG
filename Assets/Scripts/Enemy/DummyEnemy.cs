9ed72acc4dcc33f01a34f80a37d081ed30f13cee
Ivan Agarsky <agarskyivan@gmail.com>
2020-10-27 17:10:11 +0100

    Dummy enemy
    Enemy changes color after hit by frostbolt

---
+++ Assets/Scripts/Enemy/DummyEnemy.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class DummyEnemy : MonoBehaviour
+{
+    private Renderer _renderer;
+
+    private void Awake()
+    {
+        _renderer = gameObject.GetComponent<Renderer>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!gameObject.activeInHierarchy)
+            return;
+        
+        if (collision.gameObject.name == "Frostbolt Projectile(Clone)")
+        {
+            _renderer.material.color = Random.ColorHSV();
+        }
+    }
+}
